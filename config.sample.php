c958a617e2eb276372e3d6bf330b344218a35179
Brad Touesnard <brad@touesnard.com>
2014-03-31 16:05:16 -0300

    Support for multiple config files.

--- config.sample.php
+++ config.sample.php
@@ -1,6 +1,10 @@
 <?php
-$debug_log_path = realpath( dirname( __FILE__ ) . '/../public_html/wp-content/debug.log' );
+// Path to the log file
+$log_path = '/home/roger/public_html/wp-content/debug.log' );
 
+// Array of regular expressions. Each one will be run against each 
+// XDebug error and stack trace. If ANY of the regular expressions
+// match, the entire error and stack track will be filtered out
 $regexes = array(
 	'@woocommerce-delivery-notes\.php:27@' // Deprecated function in Delivery Notes addon
 );
